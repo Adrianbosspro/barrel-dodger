b837697fe9049e3cbde271c650803694ec2c1ea0
Adrianbosspro <70392728+Adrianbosspro@users.noreply.github.com>
2020-08-28 18:33:34 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -200
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
+    game.over(false, effects.starField)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010101010101010101010202020202020202020202020202020202020202`, img`
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 
+    `, [myTiles.transparency16,sprites.vehicle.roadHorizontal,sprites.dungeon.darkGroundCenter], TileScale.Sixteen))
+mySprite = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 5))
+mySprite.ay = 500
+game.onUpdateInterval(850, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 6 6 6 6 6 6 6 6 . . 
+        . . . . . 6 c 6 6 6 6 6 6 9 6 . 
+        . . . . 6 c c 6 6 6 6 6 6 9 c 6 
+        . . d 6 9 c c 6 9 9 9 9 9 9 c c 
+        . d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
+        . 6 6 6 9 b 8 8 b b b 8 b b 8 6 
+        . 6 6 6 6 6 8 b b b b 8 b b b 8 
+        . 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
+        . 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
+        . d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
+        . 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
+        . 8 8 8 8 f f f 8 8 8 8 f f f f 
+        . . . 8 f f f f f 8 8 f f f f f 
+        . . . . f f f f . . . . f f f . 
+        . . . . . . . . . . . . . . . . 
+        `, randint(-100, -80), 0)
+    tiles.placeOnTile(projectile, tiles.getTileLocation(9, 5))
+    info.changeScoreBy(1)
+    if (info.score() == 65) {
+        game.over(true, effects.confetti)
+    }
+})
